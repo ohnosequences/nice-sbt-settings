63648e0eb7df73d4bad36870b87f22e3bf49fd08
Alexey Alekhin <laughedelic@gmail.com>
2013-11-06 17:37:31 +0100

    Changed settings namespace again

--- src/main/scala/Era7SbtSettingsPlugin.scala
+++ src/main/scala/Era7SbtSettingsPlugin.scala
@@ -25,102 +25,100 @@ object Era7SettingsPlugin extends sbt.Plugin {
   object Era7 {
 
     // Sets of settings:
-    object Settings {
-      lazy val metainfoSettings: Seq[Setting[_]] = Seq(
-          homepage := Some(url("https://github.com/"+organization.value+"/"+name.value))
-        , organizationHomepage := Some(url("http://"+organization.value+".com"))
-        , licenses := Seq("AGPL-V3" -> url("http://www.gnu.org/licenses/agpl-3.0.txt"))
-        )
+    lazy val metainfoSettings: Seq[Setting[_]] = Seq(
+        homepage := Some(url("https://github.com/"+organization.value+"/"+name.value))
+      , organizationHomepage := Some(url("http://"+organization.value+".com"))
+      , licenses := Seq("AGPL-V3" -> url("http://www.gnu.org/licenses/agpl-3.0.txt"))
+      )
 
-      lazy val scalaSettings: Seq[Setting[_]] = Seq(
-        // this doesn't allow any conflicts in dependencies:
-          conflictManager := ConflictManager.strict
+    lazy val scalaSettings: Seq[Setting[_]] = Seq(
+      // this doesn't allow any conflicts in dependencies:
+        conflictManager := ConflictManager.strict
 
-        , scalaVersion := "2.10.3"
-        // 2.10.x are compatible and we want to use the latest _for everything_:
-        , dependencyOverrides += "org.scala-lang" % "scala-library" % "2.10.3"
+      , scalaVersion := "2.10.3"
+      // 2.10.x are compatible and we want to use the latest _for everything_:
+      , dependencyOverrides += "org.scala-lang" % "scala-library" % "2.10.3"
 
-        , scalacOptions ++= Seq(
-              "-feature"
-            , "-language:higherKinds"
-            , "-language:implicitConversions"
-            , "-language:postfixOps"
-            , "-deprecation"
-            , "-unchecked"
-            )
-        )
+      , scalacOptions ++= Seq(
+            "-feature"
+          , "-language:higherKinds"
+          , "-language:implicitConversions"
+          , "-language:postfixOps"
+          , "-deprecation"
+          , "-unchecked"
+          )
+      )
 
-      lazy val javaSettings: Seq[Setting[_]] = Seq(
-        // to omit _2.10 suffixes:
-          crossPaths := false
-        // to omit scala library dependency
-        , autoScalaLibrary := false
-        )
+    lazy val javaSettings: Seq[Setting[_]] = Seq(
+      // to omit _2.10 suffixes:
+        crossPaths := false
+      // to omit scala library dependency
+      , autoScalaLibrary := false
+      )
 
-      lazy val resolversSettings: Seq[Setting[_]] = Seq(
-          bucketSuffix := {organization.value + ".com"}
-        , resolvers ++= Seq ( 
-            organization.value + " public maven releases"  at 
-              toHttp("s3://releases." + bucketSuffix.value)
-          , organization.value + " public maven snapshots" at 
-              toHttp("s3://snapshots." + bucketSuffix.value)
-          // ivy
-          , Resolver.url(organization.value + " public ivy releases", 
-                         url(toHttp("s3://releases." + bucketSuffix.value)))(ivy)
-          , Resolver.url(organization.value + " public ivy snapshots", 
-                         url(toHttp("s3://snapshots." + bucketSuffix.value)))(ivy)
-          ) 
-        )
+    lazy val resolversSettings: Seq[Setting[_]] = Seq(
+        bucketSuffix := {organization.value + ".com"}
+      , resolvers ++= Seq ( 
+          organization.value + " public maven releases"  at 
+            toHttp("s3://releases." + bucketSuffix.value)
+        , organization.value + " public maven snapshots" at 
+            toHttp("s3://snapshots." + bucketSuffix.value)
+        // ivy
+        , Resolver.url(organization.value + " public ivy releases", 
+                       url(toHttp("s3://releases." + bucketSuffix.value)))(ivy)
+        , Resolver.url(organization.value + " public ivy snapshots", 
+                       url(toHttp("s3://snapshots." + bucketSuffix.value)))(ivy)
+        ) 
+      )
 
-      lazy val publishingSettings: Seq[Setting[_]] = Seq(
-          isPrivate := false
-        , publishMavenStyle := true
-        , publishBucketSuffix := bucketSuffix.value
-        , publishS3Resolver := {
-            val privacy = if (isPrivate.value) "private." else ""
-            val prefix = if (isSnapshot.value) "snapshots" else "releases"
-            val address = privacy+prefix+"."+publishBucketSuffix.value 
-            S3Resolver( 
-              name = address+" S3 publishing bucket"
-            , url = "s3://"+address
-            , patterns = if(publishMavenStyle.value) mvn else ivy
-            , overwrite = isSnapshot.value
-            )
-          }
-        , publishTo := {s3credentials.value map publishS3Resolver.value.toSbtResolver}
-        )
+    lazy val publishingSettings: Seq[Setting[_]] = Seq(
+        isPrivate := false
+      , publishMavenStyle := true
+      , publishBucketSuffix := bucketSuffix.value
+      , publishS3Resolver := {
+          val privacy = if (isPrivate.value) "private." else ""
+          val prefix = if (isSnapshot.value) "snapshots" else "releases"
+          val address = privacy+prefix+"."+publishBucketSuffix.value 
+          S3Resolver( 
+            name = address+" S3 publishing bucket"
+          , url = "s3://"+address
+          , patterns = if(publishMavenStyle.value) mvn else ivy
+          , overwrite = isSnapshot.value
+          )
+        }
+      , publishTo := {s3credentials.value map publishS3Resolver.value.toSbtResolver}
+      )
 
-      lazy val releaseSettings: Seq[Setting[_]] = 
-        ReleasePlugin.releaseSettings ++ Seq(
-          versionBump := Version.Bump.Minor
-        , tagComment  := {name.value + " v" + (version in ThisBuild).value}
-        , releaseProcess <<= thisProjectRef apply { ref =>
-            Seq[ReleaseStep](
-              checkSnapshotDependencies
-            , inquireVersions
-            , runTest
-            , setReleaseVersion
-            , commitReleaseVersion
-            , tagRelease
-            , publishArtifacts
-            , setNextVersion
-            , pushChanges
-            )
-          }
-        )
-    }
+    lazy val releaseSettings: Seq[Setting[_]] = 
+      ReleasePlugin.releaseSettings ++ Seq(
+        versionBump := Version.Bump.Minor
+      , tagComment  := {name.value + " v" + (version in ThisBuild).value}
+      , releaseProcess <<= thisProjectRef apply { ref =>
+          Seq[ReleaseStep](
+            checkSnapshotDependencies
+          , inquireVersions
+          , runTest
+          , setReleaseVersion
+          , commitReleaseVersion
+          , tagRelease
+          , publishArtifacts
+          , setNextVersion
+          , pushChanges
+          )
+        }
+      )
 
     // Global combinations of settings:
     lazy val scalaProject: Seq[Setting[_]] =
-      Settings.metainfoSettings ++
-      Settings.scalaSettings ++
-      Settings.resolversSettings ++
-      Settings.publishingSettings ++
-      Settings.releaseSettings
+      metainfoSettings ++
+      scalaSettings ++
+      resolversSettings ++
+      publishingSettings ++
+      releaseSettings
 
     lazy val javaProject: Seq[Setting[_]] =
       scalaProject ++
-      Settings.javaSettings
+      javaSettings
 
   }
 
