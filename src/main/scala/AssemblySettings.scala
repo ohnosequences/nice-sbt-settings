4bf893958959b7e4fd9d2197fab1c582fd4bfcbb
Alexey Alekhin <laughedelic@gmail.com>
2016-06-23 13:20:03 +0200

    Added fatArtifactPublish boolean setting, but conditional settings adding doesn't work

--- src/main/scala/AssemblySettings.scala
+++ src/main/scala/AssemblySettings.scala
@@ -20,45 +20,52 @@ object AssemblySettings extends sbt.Plugin {
   /* This setting holds the url of the published fat artifact */
   lazy val fatArtifactUrl = settingKey[String]("URL of the published fat artifact")
 
+  lazy val fatArtifactPublish = settingKey[Boolean]("Defines whether to publish fat jar or not")
+
   /* ### Settings
 
      Note, that these settings are not included by default. To turn them on them, add to your
      `build.sbt` `fatArtifactSettings` line (without any prefix)
   */
-  lazy val fatArtifactSettings: Seq[Setting[_]] =
-    addArtifact(artifact in (Compile, assembly), assembly) ++
-    Seq(
-      // publishing fat artifact:
-      fatArtifactClassifier := "fat",
-      artifact in (Compile, assembly) := (artifact in (Compile, assembly)).value.copy(
-        classifier = Some(fatArtifactClassifier.value)
-      ),
+  lazy val fatArtifactSettings: Seq[Setting[_]] = Seq(
+    // publishing fat artifact:
+    fatArtifactClassifier := "fat",
+    fatArtifactPublish := !(isSnapshot.value), // don't publish fat snapshots
+    artifact in (Compile, assembly) := {
+      val art = (artifact in (Compile, assembly)).value
+      art.copy( classifier = Some(fatArtifactClassifier.value) )
+    },
 
-      // turning off tests in assembly:
-      test in assembly := {},
+    // turning off tests in assembly:
+    test in assembly := {},
 
-      // mvn: "[organisation]/[module]_[scalaVersion]/[revision]/[artifact]-[revision]-[classifier].[ext]"
-      // ivy: "[organisation]/[module]_[scalaVersion]/[revision]/[type]s/[artifact]-[classifier].[ext]"
-      fatArtifactUrl := {
-        val isMvn = publishMavenStyle.value
-        val scalaV = "_"+scalaBinaryVersion.value
-        val module = moduleName.value + scalaV
-        val artifact = Seq(
-          if (isMvn) "" else "jars/",
-          module,
-          if (isMvn) "-"+version.value else "",
-          "-fat",
-          ".jar"
-        ).mkString
+    // mvn: "[organisation]/[module]_[scalaVersion]/[revision]/[artifact]-[revision]-[classifier].[ext]"
+    // ivy: "[organisation]/[module]_[scalaVersion]/[revision]/[type]s/[artifact]-[classifier].[ext]"
+    fatArtifactUrl := {
+      val isMvn = publishMavenStyle.value
+      val scalaV = "_"+scalaBinaryVersion.value
+      val module = moduleName.value + scalaV
+      val artifact = Seq(
+        if (isMvn) "" else "jars/",
+        module,
+        if (isMvn) s"-${version.value}" else "",
+        s"-${fatArtifactClassifier.value}",
+        ".jar"
+      ).mkString
 
-        Seq(
-          ResolverSettings.publishS3Resolver.value.url,
-          organization.value,
-          module,
-          version.value,
-          artifact
-        ).mkString("/")
-      }
-    )
+      Seq(
+        ResolverSettings.publishS3Resolver.value.url,
+        organization.value,
+        module,
+        version.value,
+        artifact
+      ).mkString("/")
+    }
+
+  )
+  // ++ Def.settings(
+  //   if (fatArtifactPublish.value) addArtifact(artifact in (Compile, assembly), assembly)
+  //   else seq()
+  // )
 
 }
